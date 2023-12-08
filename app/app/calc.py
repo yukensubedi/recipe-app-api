3101d33104fcab04e551711503ea6c0ed3ecac20
Yuken <yukensubedi@gmail.com>
2023-12-08 08:45:17 +0545

    Testing push and test cases

---
+++ app/app/calc.py
@@ -0,0 +1,18 @@
+
+"""
+    Calculator Function
+
+"""
+
+def add(x,y):
+    # Adds x and y and retursn the result
+    return x+y
+
+def subtract(x,y):
+    """Subtracts x and y and returns reuslt"""
+    return x-y
+
+def multiply(x,y):
+    """Multiply x and y and returns reuslt"""
+    return x*y
+

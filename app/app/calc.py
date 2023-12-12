e69a8e30f51ccc43a3a8aedcb4f3dc3659dd1704
Yuken <yukensubedi@gmail.com>
2023-12-12 10:15:25 +0545

    fixed linting issues and test cases added for DB

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,18 +1,18 @@
-
 """
-    Calculator Function
-
+Calculator Function
 """
 
-def add(x,y):
-    # Adds x and y and retursn the result
-    return x+y
 
-def subtract(x,y):
-    """Subtracts x and y and returns reuslt"""
-    return x-y
+def add(x, y):
+    # Adds x and y and returns the result
+    return x + y
 
-def multiply(x,y):
-    """Multiply x and y and returns reuslt"""
-    return x*y
 
+def subtract(x, y):
+    """Subtracts x and y and returns the result"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply x and y and returns the result"""
+    return x * y
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,8 +3,8 @@ Sample Tests
 """
 
 from django.test import SimpleTestCase
-
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """
@@ -13,18 +13,10 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding numbers together"""
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
-        res = calc.add(5,6)
-        self.assertEqual(res,11)
-    
     def test_subtract_numbers(self):
-        """test subtracting numbers """
-
-        res = calc.subtract(10,15)
-
+        """Test subtracting numbers"""
+        res = calc.subtract(10, 15)
         self.assertEqual(res, -5)
-    
-    
-
-
-    

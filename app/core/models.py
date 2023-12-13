4565e98012419d2f35589d1614ed7b9e4455adfb
Yuken <yukensubedi@gmail.com>
2023-12-13 07:55:16 +0545

    fixed linting errors

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 """
-Database models 
+Database models
 """
-from django.db import models 
+from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -10,20 +10,20 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-    """ Manager for users """
+    """Manager for users"""
 
     def create_user(self, email, password=None, **extra_fields):
-        """ Create, save and return a new user """
+        """Create, save and return a new user"""
         if not email:
-            raise ValueError('User musy have an email address')
+            raise ValueError('User must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password):
-        """ Create and return a new superuser """
+        """Create and return a new superuser"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -33,7 +33,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """ User in the system """
+    """User in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -42,4 +42,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,5 +1,5 @@
-""" 
-Tests for the django admin modification 
+"""
+Tests for the django admin modification
 """
 
 from django.test import TestCase
@@ -7,25 +7,26 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import Client
 
+
 class AdminSiteTests(TestCase):
-    """ Tests for Django admin"""
+    """Tests for Django admin"""
 
     def setUp(self):
-        """ Create user and client """
+        """Create user and client"""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@example.com', 
+            email='admin@example.com',
             password='testpass123'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'user@example.com',
-            password = 'testpass123',
-            name = 'Test User'
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
         )
 
-    def test_users_lists(self):
-        """Test that users are listed on page."""
+    def test_users_list(self):
+        """Test that users are listed on the page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
@@ -33,14 +34,14 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.email)
 
     def test_edit_user_page(self):
-        """ Test the edit user page works """
+        """Test the edit user page works"""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
-        """ Tests the create user page"""
+        """Tests the create user page"""
         url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
